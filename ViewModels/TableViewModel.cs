1540b6de3d504f22da2f57f9a6d39892cef894d4
duyd486 <duyd486@gmail.com>
2025-03-18 16:39:33 +0700

    add

--- ViewModels/TableViewModel.cs
+++ ViewModels/TableViewModel.cs
@@ -11,17 +11,19 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Runtime.CompilerServices;
 using Microsoft.IdentityModel.Tokens;
+using QuanLiQuanAn.Views;
+using System.Windows.Input;
 
 namespace QuanLiQuanAn.ViewModels
 {
     public partial class TableViewModel : ObservableObject
     {
         [ObservableProperty]
-        private ObservableCollection<Employee> employeeOb;
+        private ObservableCollection<Table> tableOb;
 
         public ObservableCollection<string> SortItems { get; } =
         [
-        "All", "Table For 2", "Table For 4", "Above 4"
+        "All", "Table For 2", "Table For 3", "Table For 4", "Above 4"
         ];
 
         [ObservableProperty]
@@ -31,36 +33,15 @@ namespace QuanLiQuanAn.ViewModels
         [
             "Male" , "Female"
         ];
-        public IEnumerable<string> City { get; } =
-        [
-            "An Giang", "Ba Ria - Vung Tau", "Bac Lieu", "Bac Giang", "Bac Kan", "Bac Ninh",
-            "Ben Tre", "Binh Duong", "Binh Dinh", "Binh Phuoc", "Binh Thuan", "Ca Mau",
-            "Cao Bang", "Can Tho", "Da Nang", "Dak Lak", "Dak Nong", "Dien Bien", "Dong Nai",
-            "Dong Thap", "Gia Lai", "Ha Giang", "Ha Nam", "Ha Noi", "Ha Tinh", "Hai Duong",
-            "Hai Phong", "Hau Giang", "Hoa Binh", "Ho Chi Minh", "Hung Yen", "Khanh Hoa",
-            "Kien Giang", "Kon Tum", "Lai Chau", "Lang Son", "Lao Cai", "Lam Dong",
-            "Long An", "Nam Dinh", "Nghe An", "Ninh Binh", "Ninh Thuan", "Phu Tho",
-            "Phu Yen", "Quang Binh", "Quang Nam", "Quang Ngai", "Quang Ninh", "Quang Tri",
-            "Soc Trang", "Son La", "Tay Ninh", "Thai Binh", "Thai Nguyen", "Thanh Hoa",
-            "Thua Thien Hue", "Tien Giang", "Tra Vinh", "Tuyen Quang", "Vinh Long",
-            "Vinh Phuc", "Yen Bai"
-        ];
-
 
         [ObservableProperty]
         private string? selectedSortItem;
 
-        [ObservableProperty]
-        private string? nameSearch = "";
-        partial void OnNameSearchChanged(string? value)
-        {
-            SearchByName(value);
-        }
 
 
         public TableViewModel()
         {
-            EmployeeOb = new ObservableCollection<Employee>();
+            TableOb = new ObservableCollection<Table>();
             _ = Loading();
             selectedSortItem = "All";
         }
@@ -82,231 +63,156 @@ namespace QuanLiQuanAn.ViewModels
         {
             QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db = new QuanannhatContext();
             await Task.Delay(1000);
-            EmployeeOb.Clear();
-            foreach (Employee employee in db.Employees.OrderByDescending(e => e.Id).Include(e => e.Information).ToList())
+            TableOb.Clear();
+            foreach (Table table in db.Tables.OrderByDescending(e => e.Id))
             {
-                EmployeeOb.Add(employee);
+                TableOb.Add(table);
             }
 
         }
         [RelayCommand]
-        private void SortRole(string roleStr)
+        private void SortContain(string containStr)
         {
-            SelectedSortItem = roleStr;
-            int role = 0;
-            switch (roleStr)
+            SelectedSortItem = containStr;
+            int contain = 0;
+            switch (containStr)
             {
                 case "All":
                     _ = GetAll();
                     return;
-                case "Waitress":
-                    role = 1;
+                case "Table For 2":
+                    contain = 2;
                     break;
-                case "Cashier":
-                    role = 2;
+                case "Table For 3":
+                    contain = 3;
                     break;
-                case "Chef":
-                    role = 3;
+                case "Table For 4":
+                    contain = 4;
+                    break;
+                default:
+                    contain = 0;
                     break;
             }
             QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db;
-            EmployeeOb.Clear();
-            foreach (Employee employee in db.Employees.Include(e => e.Information).ToList())
+            TableOb.Clear();
+            foreach (Table table in db.Tables)
             {
-                if (employee.Role == role)
+                if(contain != 0)
                 {
-                    EmployeeOb.Add(employee);
+                    if (table.Contain == contain)
+                    {
+                        TableOb.Add(table);
+                    }
+                } else
+                {
+                    if(table.Contain > 4)
+                    {
+                        TableOb.Add(table);
+                    }
                 }
+                
+
             }
         }
 
 
         [ObservableProperty]
-        Employee? employeeTmp;
-        [ObservableProperty]
-        Information informationTmp;
-        [ObservableProperty]
-        private DateOnly date = DateOnly.FromDateTime(DateTime.Today);
-        private Views.AddEmployee addEmployeeView;
+        Table? tableTmp;
+        private AddTableView addTableView;
         private bool isEdit;
         [RelayCommand]
-        private void InteractEmployee(Employee employee)
+        private void InteractTable(Table table)
         {
             isEdit = true;
-            EmployeeTmp = new();
-            EmployeeTmp = employee;
-            InformationTmp = new();
-            InformationTmp = employee.Information;
+            TableTmp = new();
+            TableTmp = table;
 
-            addEmployeeView = new();
-            addEmployeeView.DataContext = this;
-            addEmployeeView.ShowDialog();
+            addTableView = new();
+            addTableView.DataContext = this;
+            addTableView.ShowDialog();
         }
 
         [RelayCommand]
-        private void AddEmployee()
+        private void AddTable()
         {
             isEdit = false;
-            InformationTmp = new();
-            EmployeeTmp = new();
-            addEmployeeView = new();
-            addEmployeeView.DataContext = this;
-            addEmployeeView.ShowDialog();
+            TableTmp = new();
+            addTableView = new();
+            addTableView.DataContext = this;
+            addTableView.ShowDialog();
         }
         [RelayCommand]
-        private void ApplyEmployee(string sender)
+        private void ApplyTable(string sender)
         {
             if (sender == "Apply")
             {
                 if (!isEdit)
                 {
-                    SaveNewEmployee();
+                    SaveNewTable();
+                    _ = GetAll();
+                    MessageBox.Show("Them thanh cong");
                 }
                 else
                 {
-                    UpdateEmployee();
+                    UpdateTable();
+                    _ = GetAll();
+                    MessageBox.Show("Sua thanh cong");
                 }
             }
-            _ = GetAll();
-            addEmployeeView.Close();
+            addTableView.Close();
         }
 
         [RelayCommand]
-        private void ImportExcel()
+        private void DeleteTable(Table table)
         {
-            string filePath = "";
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            if (openFileDialog.ShowDialog() == true)
-            {
-                filePath = openFileDialog.FileName;
-            }
-            if (filePath == "")
-            {
-                return;
-            }
-            try
-            {
-                QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db = new QuanannhatContext();
-
-                var package = new ExcelPackage(new FileInfo(filePath));
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-
-                for (int i = worksheet.Dimension.Start.Row + 1; i <= worksheet.Dimension.End.Row; i++)
-                {
-                    SalaryBill salaryBill = new SalaryBill();
-                    salaryBill.Id = db.SalaryBills.IsNullOrEmpty() ? 1 : db.SalaryBills.OrderBy(e => e.Id).Last().Id + 1;
-
-                    int employeeIdCollum = 1;
-                    salaryBill.EmployeeId = Convert.ToInt32(worksheet.Cells[i, employeeIdCollum].Value);
-
-                    int totalShiftCollum = 3;
-                    salaryBill.TotalShifts = Convert.ToInt32(worksheet.Cells[i, totalShiftCollum].Value);
-
-                    salaryBill.Status = 1;
-
-                    int timeCollum = 7;
-                    string? timeStr = worksheet.Cells[i, timeCollum].Value.ToString();
-                    salaryBill.Time = DateOnly.Parse(timeStr.Substring(0, timeStr.IndexOf(" ")));
-
-                    db.SalaryBills.Add(salaryBill);
-                    db.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            Console.WriteLine("Delete Table");
+            QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db = new QuanannhatContext();
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            db.Entry(table).State = EntityState.Detached;
+            _ = GetAll();
+            MessageBox.Show("Delete Completed");
 
         }
 
-
-
-        private void UpdateEmployee()
+        private void UpdateTable()
         {
             try
             {
                 QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db = new QuanannhatContext();
-                if (EmployeeTmp.Salary.ToString() == "")
+                if (TableTmp.Contain == null)
                 {
-                    Exception exception = new Exception("Salary was write in wrong condiction");
+                    Exception exception = new Exception("Contain is emty");
                     throw exception;
                 }
-                else if (InformationTmp.CitizenId.ToString() == "")
-                {
-                    Exception exception = new Exception("Cityzen Id was write in wrong condiction");
-                    throw exception;
-                }
-                else if (InformationTmp.Name == "")
-                {
-                    Exception exception = new Exception("Name is emty");
-                    throw exception;
-                }
-
-                db.Informations.Update(InformationTmp);
-                db.Employees.Update(EmployeeTmp);
+                db.Tables.Update(TableTmp);
                 db.SaveChanges();
-                db.Entry(InformationTmp).State = EntityState.Detached;
-
-                MessageBox.Show("Sua thanh cong");
-
+                db.Entry(TableTmp).State = EntityState.Detached;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
         }
 
-        private void SaveNewEmployee()
+        private void SaveNewTable()
         {
             try
             {
                 QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db;
-                InformationTmp.Id = db.Informations.IsNullOrEmpty() ? 1 : db.Informations.OrderBy(e => e.Id).Last().Id + 1;
-                EmployeeTmp.Id = db.Employees.IsNullOrEmpty() ? 1 : db.Employees.OrderBy(e => e.Id).Last().Id + 1;
-                EmployeeTmp.InformationId = InformationTmp.Id;
-                InformationTmp.Birth = Date;
+                TableTmp.Id = db.Tables.IsNullOrEmpty() ? 1 : db.Tables.OrderBy(e => e.Id).Last().Id + 1;
 
-                if (EmployeeTmp.Salary.ToString() == "")
+                if (TableTmp.Contain.ToString() == "")
                 {
-                    Exception exception = new Exception("Salary was write in wrong condiction");
+                    Exception exception = new Exception("Contain is emty");
                     throw exception;
                 }
-                else if (InformationTmp.CitizenId.ToString() == "")
-                {
-                    Exception exception = new Exception("Cityzen Id was write in wrong condiction");
-                    throw exception;
-                }
-                else if (InformationTmp.Name == "")
-                {
-                    Exception exception = new Exception("Name is emty");
-                    throw exception;
-                }
-
-                db.Informations.Add(InformationTmp);
-                db.Employees.Add(EmployeeTmp);
+                db.Tables.Add(TableTmp);
                 db.SaveChanges();
-                MessageBox.Show("Them thanh cong");
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-
-        }
-
-        private void SearchByName(string name)
-        {
-            QuanannhatContext db = Singleton.DatabaseSingleton.GetInstance().db;
-            EmployeeOb.Clear();
-            foreach (Employee employee in db.Employees.Include(e => e.Information).ToList())
-            {
-                if (employee.Information.Name.Contains(name, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    EmployeeOb.Add(employee);
-                }
             }
         }
 
